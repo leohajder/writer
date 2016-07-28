a050a8fd9ce11221f1f82be8a2716613cc760887
lhajder <leohajder@gmail.com>
2016-07-28 05:46:20 +0200

    "Saved" flag added
    Writer will ask to save the file only if it has been changed. Otherwise
    there will be no popups.

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-A simple text editor application made for fun and practice with Python 3.5.2 using the Tkinter module for GUI programming.<br/>
-Author: Leo Hajder (github.com/lhajder)
+# Writer
 
-![Screenshot](screenshot.png)
+A simple text editor application made for fun and practice with Python 3.5.2 using the Tkinter module for GUI programming.
--- writer.py
+++ writer.py
@@ -1,24 +1,28 @@
 from tkinter import *
 
 filename = None
+saved = True
 
 def fNew(*args):
-    global filename
-    result = messagebox.askyesnocancel("Save file?", "Do you want to save the current file?")
-    if(result == True):
-        fSave()
-    elif(result == None):
-        return
+    global filename, saved
+    if(saved == False):
+        result = messagebox.askyesnocancel("Save file?", "Do you want to save the current file?")
+        if(result == True):
+            fSave()
+        elif(result == None):
+            return
     filename = None
     text.delete(0.0, END)
+    saved = True
     
 def fOpen(*args):
-    global filename
-    result = messagebox.askyesnocancel("Save file?", "Do you want to save the current file?")
-    if(result == True):
-        fSave()
-    elif(result == None):
-        return
+    global filename, saved
+    if(saved == False):
+        result = messagebox.askyesnocancel("Save file?", "Do you want to save the current file?")
+        if(result == True):
+            fSave()
+        elif(result == None):
+            return
     try:
         f = filedialog.askopenfile(filetypes = [('text files', '.txt'),('all files', '.*')])
         if(f):
@@ -26,37 +30,48 @@ def fOpen(*args):
             t = f.read()
             text.delete(0.0, END)
             text.insert(END, t)
+            saved = True
     except:
         messagebox.showerror("Error", "Unable to open file.")
       
 def fSave(*args):
-    global filename
+    global filename, saved
     t = text.get(0.0, END)
     if(filename):
         f = open(filename, "w")
         f.write(t)
         f.close()
+        saved = True
     else:
         fSaveAs()
 
 def fSaveAs(*args):
+    global saved
     f = filedialog.asksaveasfile(defaultextension=".txt", filetypes = [('text files', '.txt'),('all files', '.*')])
     t = text.get(0.0, END)
     if(f):
         try:
             f.write(t)
             f.close()
+            saved = True
         except:
             messagebox.showwarning("Error", "Unable to save file.")
 
 def onExit(*args):
-    result = messagebox.askyesnocancel("Save file?", "Do you want to save the current file?")
-    if(result == True):
-        fSave()
-    elif(result == None):
-        return
+    global saved
+    if(saved == False):
+        result = messagebox.askyesnocancel("Save file?", "Do you want to save the current file?")
+        if(result == True):
+            fSave()
+        elif(result == None):
+            return
     writer.destroy()
-    
+
+def setSavedFalse(key):
+    global saved
+    if(key.keysym.isalpha() or key.keysym.isdigit() or key.keysym in ["Return", "Tab", "Backspace", "Delete"]): #any key that changes text
+        saved = False
+       
 #initialization
 writer = Tk()
 writer.title("Writer")
@@ -72,7 +87,7 @@ filemenu.add_command(label = "Open", command = fOpen)
 filemenu.add_command(label = "Save", command = fSave)
 filemenu.add_command(label = "Save As", command = fSaveAs)
 filemenu.add_separator()
-filemenu.add_command(label = "About", command = lambda: messagebox.showinfo("About", "A simple text editor made for fun and practice with Python 3.5.2 using the Tkinter module for GUI programming. \nAuthor: Leo Hajder (github.com/lhajder)"))
+filemenu.add_command(label = "About", command = lambda: messagebox.showinfo("About", "A simple text editor made for fun and practice with Python 3.5.2 using the Tkinter module for GUI programming. \nAuthor: Leo Hajder (leohajder.github.io)"))
 filemenu.add_separator()
 filemenu.add_command(label = "Exit", command = onExit)
 menubar.add_cascade(label = "File", menu = filemenu)
@@ -82,6 +97,7 @@ writer.config(menu=menubar)
 writer.bind('<Control-n>', fNew)
 writer.bind('<Control-o>', fOpen)
 writer.bind('<Control-s>', fSave)
+writer.bind('<Key>', setSavedFalse)
 
 #save before exit?
 writer.protocol("WM_DELETE_WINDOW", onExit)
