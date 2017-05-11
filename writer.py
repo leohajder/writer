88141ae01ee863467325edb396b85b37d34f945c
Leo Hajder <leohajder@gmail.com>
2017-05-11 05:10:27 +0200

    Refactor and reformat to PEP 8

--- writer.py
+++ writer.py
@@ -2,56 +2,62 @@ from tkinter import *
 from tkinter import messagebox
 from tkinter import filedialog
 
-filename = None
+fileName = None
 saved = True
 
-def fNew(*args):
-    global filename, saved
-    if(saved == False):
+
+def newFile(*args):
+    global fileName, saved
+    if not saved:
         save = promptToSave()
-        if(save == True):
-            fSave()
-        elif(save == None):
+        if save:
+            saveFile()
+        elif save is None:
             return
-    filename = None
+    fileName = None
     text.delete(0.0, END)
     saved = True
-    
-def fOpen(*args):
-    global filename, saved
-    if(saved == False):
+
+
+def openFile(*args):
+    global fileName, saved
+    if not saved:
         save = promptToSave()
-        if(save == True):
-            fSave()
-        elif(save == None):
+        if save:
+            saveFile()
+        elif save is None:
             return
     try:
-        f = filedialog.askopenfile(filetypes = [('text files', '.txt'),('all files', '.*')])
-        if(f):
-            filename = f.name
+        f = filedialog.askopenfile(
+            filetypes=[('text files', '.txt'), ('all files', '.*')])
+        if f:
+            fileName = f.name
             t = f.read()
             text.delete(0.0, END)
             text.insert(END, t)
             saved = True
     except:
         messagebox.showerror("Error", "Unable to open file.")
-      
-def fSave(*args):
-    global filename, saved
+
+
+def saveFile(*args):
+    global fileName, saved
     t = text.get(0.0, END)
-    if(filename):
-        f = open(filename, "w")
+    if fileName:
+        f = open(fileName, "w")
         f.write(t)
         f.close()
         saved = True
     else:
-        fSaveAs()
+        saveFileAs()
 
-def fSaveAs(*args):
+
+def saveFileAs(*args):
     global saved
-    f = filedialog.asksaveasfile(defaultextension=".txt", filetypes = [('text files', '.txt'),('all files', '.*')])
+    f = filedialog.asksaveasfile(defaultextension=".txt", filetypes=[
+                                 ('text files', '.txt'), ('all files', '.*')])
     t = text.get(0.0, END)
-    if(f):
+    if f:
         try:
             f.write(t)
             f.close()
@@ -59,57 +65,63 @@ def fSaveAs(*args):
         except:
             messagebox.showwarning("Error", "Unable to save file.")
 
+
 def onExit(*args):
     global saved
-    if(saved == False):
+    if not saved:
         save = promptToSave()
-        if(save == True):
-            fSave()
-        elif(save == None):
+        if save:
+            saveFile()
+        elif save is None:
             return
     app.destroy()
 
+
 def setSavedFalse(key):
     global saved
-    if(key.keysym.isalpha() or key.keysym.isdigit() or key.keysym in ["Return", "Tab", "Backspace", "Delete"]): #any key that changes text
+    if (key.keysym.isalpha() or key.keysym.isdigit() or key.keysym in ["Return", "Tab", "Backspace",
+                                                                       "Delete"]):  # any key that changes text
         saved = False
-        
-def promptToSave():
-       return messagebox.askyesnocancel("Save file?", "Do you want to save the current file?")
 
-#initialization
+
+def promptToSave():
+    return messagebox.askyesnocancel("Save file?", "Do you want to save the current file?")
+
+
+# initialization
 app = Tk()
 app.title("Writer")
 
-#initializing text container
+# initializing text container
 text = Text(app)
 
 about = "A simple text editor in Python 3.5.2 using the Tkinter module for GUI programming. \nAuthor: Leo Hajder (leohajder.github.io)"
 
-#menu
-menubar = Menu(app)
-filemenu = Menu(menubar, tearoff = 0)
-filemenu.add_command(label = "New", command = fNew)
-filemenu.add_command(label = "Open", command = fOpen)
-filemenu.add_command(label = "Save", command = fSave)
-filemenu.add_command(label = "Save As", command = fSaveAs)
-filemenu.add_separator()
-filemenu.add_command(label = "About", command = lambda: messagebox.showinfo("About", about))
-filemenu.add_separator()
-filemenu.add_command(label = "Exit", command = onExit)
-menubar.add_cascade(label = "File", menu = filemenu)
-app.config(menu=menubar)
+# menu
+menuBar = Menu(app)
+fileMenu = Menu(menuBar, tearoff=0)
+fileMenu.add_command(label="New", command=newFile)
+fileMenu.add_command(label="Open", command=openFile)
+fileMenu.add_command(label="Save", command=saveFile)
+fileMenu.add_command(label="Save As", command=saveFileAs)
+fileMenu.add_separator()
+fileMenu.add_command(
+    label="About", command=lambda: messagebox.showinfo("About", about))
+fileMenu.add_separator()
+fileMenu.add_command(label="Exit", command=onExit)
+menuBar.add_cascade(label="File", menu=fileMenu)
+app.config(menu=menuBar)
 
-#key Bindings
-app.bind('<Control-n>', fNew)
-app.bind('<Control-o>', fOpen)
-app.bind('<Control-s>', fSave)
+# key Bindings
+app.bind('<Control-n>', newFile)
+app.bind('<Control-o>', openFile)
+app.bind('<Control-s>', saveFile)
 app.bind('<Key>', setSavedFalse)
 
-#save before exit?
+# save before exit?
 app.protocol("WM_DELETE_WINDOW", onExit)
 
-#deploying text container
+# deploying text container
 text.pack(expand=True, fill='both')
 
 text.focus()
